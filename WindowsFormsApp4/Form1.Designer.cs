e2dcf87672b2ee8983926b3143b4bc23774c5104
daothinhduc2005 <daothinhduc2005@gmail.com>
2025-05-18 13:14:02 +0700

    Bai1

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.rtbHTML = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(27, 26);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(600, 22);
+            this.txtURL.TabIndex = 0;
+            this.txtURL.Text = "http://";
+            this.txtURL.TextChanged += new System.EventHandler(this.txtURL_TextChanged);
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(641, 26);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(99, 23);
+            this.btnGet.TabIndex = 1;
+            this.btnGet.Text = "GET";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // rtbHTML
+            // 
+            this.rtbHTML.Location = new System.Drawing.Point(27, 85);
+            this.rtbHTML.Name = "rtbHTML";
+            this.rtbHTML.Size = new System.Drawing.Size(713, 338);
+            this.rtbHTML.TabIndex = 2;
+            this.rtbHTML.Text = "";
+            this.rtbHTML.TextChanged += new System.EventHandler(this.rtbHTML_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbHTML);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.txtURL);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.RichTextBox rtbHTML;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Khởi tạo URL mặc định khi load form
+            txtURL.Text = "http://";
+        }
+
+        // Khi người dùng nhấn nút GET
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            string url = txtURL.Text.Trim();
+            if (string.IsNullOrEmpty(url))
+            {
+                MessageBox.Show("Vui lòng nhập URL!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Nếu chưa có http:// hoặc https:// thì thêm vào
+            if (!url.StartsWith("http://", StringComparison.OrdinalIgnoreCase) &&
+                !url.StartsWith("https://", StringComparison.OrdinalIgnoreCase))
+            {
+                url = "http://" + url;
+            }
+
+            try
+            {
+                // Lấy HTML và hiển thị lên RichTextBox
+                string html = GetHTML(url);
+                rtbHTML.Text = html;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi tải trang:\n" + ex.Message,
+                                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Hàm gọi WebRequest/WebResponse để đọc về HTML
+        private string GetHTML(string szURL)
+        {
+            WebRequest request = WebRequest.Create(szURL);
+            using (WebResponse response = request.GetResponse())
+            using (Stream dataStream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(dataStream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        // (Nếu bạn có cần xử lý khi người dùng sửa TextBox hoặc RichTextBox,
+        // có thể để nguyên hoặc xoá 2 handler sau)
+        private void txtURL_TextChanged(object sender, EventArgs e)
+        {
+            // Không cần làm gì thêm
+        }
+
+        private void rtbHTML_TextChanged(object sender, EventArgs e)
+        {
+            // Không cần làm gì thêm
+        }
+    }
+}
